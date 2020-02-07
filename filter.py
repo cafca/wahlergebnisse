8a362d96e27242f727293acd6a560705af540a27
Vincent Ahrend <mail@vincentahrend.com>
2020-02-07 17:44:59 +0100

    Refactor for new Tagesschau-Wahlarchiv

--- filter.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('wahlergebnisse.json') as f:
-    data = json.load(f)
-
-df = filter(lambda x: int(x['date'][:4]) >= 2002, data)
-
-for entry in df:
-    print(entry['title'])
--- get_data.sh
+++
@@ -1,1 +0,0 @@
-curl "https://wahl.tagesschau.de/recherche/recherche.shtml?case=has&wahltyp=0&partei1=Union%2CCDU%2CCSU&op=m&procent1=0" -o data.html
